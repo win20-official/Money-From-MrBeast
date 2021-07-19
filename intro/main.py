0faa1271c2d4a9a04eb99a6eb0c9d0e654eb75d9
Windows 20 <80408876+win20-official@users.noreply.github.com>
2021-07-19 21:16:25 +0200

    Add files via upload

---
+++ intro/main.py
@@ -0,0 +1,4 @@
+print("Hello to MrBeast money game")
+name = input("What is your name? ")
+print(f"OK, {name} let's start")
+input()
---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+os.system('%cd%/intro/main.py')
+os.system('cls')
+money = input("Do you want get MrBeast money? y or n: ")
+if money == 'y':
+    print("MrBeast have 0$")
+    print("and you have 100,000,000$")
+    input()
+if money == 'n':
+    print("MrBeast have 100,000,000$")
+    print("and you have 0$")
+    input()
