19dc7cb4f1dc412b22c392247506081e5de08f0a
Windows 20 <kasiak0101@gmail.com>
2022-05-14 13:07:58 +0200

    Update code

---
+++ getpythonfiles/getbypath.py
@@ -0,0 +1,4 @@
+import os as o
+
+def path(file):
+    o.system(f"python {file}")
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import os
-os.system('%cd%/intro/main.py')
-os.system('cls')
-money = input("Do you want get MrBeast money? y or n: ")
-if money == 'y':
+import os as o
+from getpythonfiles import getbypath
+
+path = o.getcwd()
+getbypath.path(f"{path}/intro/main.py")
+o.system("cls")
+
+money = input("Do you want get MrBeast money? yes or no: ")
+
+if money == 'yes':
     print("MrBeast have 0$")
     print("and you have 100,000,000$")
     input()
-if money == 'n':
+
+if money == 'no':
     print("MrBeast have 100,000,000$")
     print("and you have 0$")
     input()
